50a42cc82640021603b20544bec12a591d83d225
tuyenhoang0320i1 <hoangtuyen9x@gmail.com>
2020-05-19 16:13:50 +0700

    add file lab

--- src/Cylinder.php
+++ src/Cylinder.php
@@ -10,9 +10,19 @@ class Cylinder
 {
     public function getVolume($radius, $height)
     {
-        $baseArea = pi() * $radius * $radius;
-        $perimeter = 2 * pi() * $radius;
+        $baseArea = $this->getBaseArea($radius);
+        $perimeter = $this->getPerimeter($radius);
         $volume = $perimeter * $height + 2 * $baseArea;
         return $volume;
+    }
+
+    public function getBaseArea($radius)
+    {
+        return pi() * $radius * $radius;
+    }
+
+    public function getPerimeter($radius)
+    {
+        return 2 * pi() * $radius;
     }
 }
